6013abd0a62716580b98af6a66a5ac83a87c2c03
Kalyan Kanakatla <kalyan.kanakatla@gmail.com>
2016-07-03 21:35:18 -0500

    Included ignoure file. Did changes to OneWaySearch class added wanted in comments section.

--- One-way/src/main/java/com/kalyan/Airline/Search/OneWaySearch.java
+++ One-way/src/main/java/com/kalyan/Airline/Search/OneWaySearch.java
@@ -3,7 +3,7 @@ package com.kalyan.Airline.Search;
 public class OneWaySearch {
 	
 	public String searchOneWay(String src){
-		return "Hi you travaeling to "+src;
+		return "Hi you wanted travaeling to "+src;
 	}
 
 }
